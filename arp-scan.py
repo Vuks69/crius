fd43cabc2ae1a8a5f9dc41df12fb9a4affe3bc84
Vuks69 <51289041+Vuks69@users.noreply.github.com>
2022-12-14 13:13:43 +0100

    formatting, shebang

--- arp-scan.py
+++ arp-scan.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import subprocess
 import json
 import sys
@@ -7,7 +9,9 @@ arp_scan_args = ["arp-scan", "--localnet"]
 if len(sys.argv) > 1:
     arp_scan_args.append("--interface=" + sys.argv[1])
 
-process = subprocess.Popen(arp_scan_args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+process = subprocess.Popen(
+    arp_scan_args, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+)
 out, err = process.communicate()
 parsed = out.decode().split("\n")[2:-4]
 
@@ -19,21 +23,23 @@ for row in parsed:
     split_row = row.split("\t")
     ip_addr, mac_addr, vendor_details = split_row
     if len(sys.argv) > 1:
-        arp_fingerprint = "arp-fingerprint -o \"-N -I " + sys.argv[1] + "\" " + ip_addr
+        arp_fingerprint = 'arp-fingerprint -o "-N -I ' + sys.argv[1] + '" ' + ip_addr
     else:
         arp_fingerprint = "arp-fingerprint " + ip_addr
-    process = subprocess.Popen(arp_fingerprint, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    process = subprocess.Popen(
+        arp_fingerprint, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+    )
     out, err = process.communicate()
     fingerprint, os = out.decode().strip().split("\t")[1:]
 
     info_dict.update(
-        {i:
-            {
+        {
+            i: {
                 "IP Address": ip_addr,
                 "Hardware Address": mac_addr,
                 "Vendor Details": vendor_details,
                 "Fingerprint": fingerprint,
-                "Operating system": os
+                "Operating system": os,
             }
         }
     )
@@ -42,5 +48,5 @@ for row in parsed:
 
 json_string = json.dumps(info_dict, indent=4)
 
-with open('json_data.json', 'w') as outfile:
+with open("json_data.json", "w") as outfile:
     outfile.write(json_string)
